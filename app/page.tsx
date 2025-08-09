c1ac9b77d7f73e5ec287b3263c499847937997fd
abdullah al sazib <mdsazibhossin2021@gmail.com>
2025-08-09 18:54:40 +0600

    refactor: rename project and update dependencies
    - Changed project name from "flowbite-react-template-nextjs" to "e-vendor-website".
    - Updated Next.js version from 15.4.2 to 15.4.6.
    - Removed unused scripts from package.json.
    - Updated React and React-DOM versions to 19.1.0.
    - Updated TypeScript and type definitions to more recent versions.
    - Removed prettier configuration file as it is no longer needed.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
+function page() {
+  return <>page</>;
 }
 
-export default page
+export default page;
